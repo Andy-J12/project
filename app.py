f728f8e690cd1e1a295ea6e63e85d045c89bf610
Andy-J12 <153670915+Andy-J12@users.noreply.github.com>
2024-12-08 18:47:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
+import uvicorn
 
+# Crear la app FastAPI
 app = FastAPI()
 
-# Carga tu modelo
+# Permitir el acceso desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas las URLs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Cargar el modelo
 model = joblib.load("model.pkl")
 
 @app.get("/")
@@ -12,7 +25,10 @@ def read_root():
 
 @app.post("/predict")
 def predict(data: dict):
-    # Suponiendo que los datos son un diccionario con las características
-    X = [data['feature1'], data['feature2'], data['feature3']]  # Ajusta las características
+    X = [data['age'], data['duration'], data['balance'], data['pdays']]
     prediction = model.predict([X])
     return {"prediction": int(prediction[0])}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto proporcionado por el entorno, por defecto 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
